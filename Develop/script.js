72e08bb632222009963e19c05d3386629775b4a4
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-03-02 22:38:11 -0600

    Loop creates, styles, & appends blocks.

--- Develop/script.js
+++ Develop/script.js
@@ -3,44 +3,51 @@
 
 // 2. Use Moment.js and use correct format to display current day.
 var currentDay = moment().format("dddd");
-var dayContainer = $('#currentDayInWeek');
-var container = $('#blocks');
+var dayContainer = $("#currentDayInWeek");
+var container = $("#blocks");
 
 // 1. Create function that runs on page load.
-function displayDay(){
-    // 3. Inside of this function, add current day to p tag.
-    $(dayContainer).html(`${currentDay}`);
-};
+function displayDay() {
+  // 3. Inside of this function, add current day to p tag.
+  $(dayContainer).html(`${currentDay}`);
+}
 
 displayDay();
 
-
 // DISPLAY TIMEBLOCKS FOR BIZ HOURS (9A-5P). 1 BLOCK PER HOUR
 // ----------------------
-// 
+function createBlocks() {
+  // 1. Loop to create timeblocks w/ class "block".
+  console.log("Hey it started.");
+  var hours = [
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4PM",
+  ];
+  for (var i = 0; i < hours.length; i++) {
+    var blockLabel = $("<label></label>");
+    $(blockLabel).html(hours[i]);
+    var newBlock = $('<div class="block">');
+    $(container).append(blockLabel);
+    $(container).append(newBlock);
+    // Applies default styling to new HTML elements.
+    $(".block").css({
+      height: "50px",
+      width: "85vw",
+      margin: "10px 10px",
+      "background-color": "grey",
+    });
+  }
 
-function createBlocks(){
-    // 1. Loop to create timeblocks w/ class "block".
-    console.log("Hey it started.")
-    var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4PM"];
-    for (var i = 0; i < hours.length; i++) {
-        var blockLabel = $('<label></label>')
-        $(blockLabel).html(hours[i]);
-        var newBlock = $('<div class="block">')
-        $(container).append(blockLabel);
-        $(container).append(newBlock);
-    }
-
-
-    console.log("Hey it worked.")
-};
+  console.log("Hey it worked.");
+}
 
 createBlocks();
-
-
-
-
-
 
 // Clicking timeblock allows for user input of an event
 // ----------------------
@@ -55,12 +62,11 @@ createBlocks();
 // 3. On click, save user input to local storage.
 // 4. On click, close the modal.
 
-
 // If app is refreshed, these events remain in place
 // ----------------------
-// 1. 
+// 1.
 
 // Style timeblocks based on past, present, or future.
 // ----------------------
 // var currentTime = moment().format("LTS");
-// 1. 
+// 1.
