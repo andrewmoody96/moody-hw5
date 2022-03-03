13a541b66f1e7f2c00a6990abe4f6ac6b9c36b92
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-03-02 20:08:28 -0600

    Initial commit

---
+++ Develop/script.js
@@ -0,0 +1,45 @@
+// Display current day using Moment.js when app is loaded
+// ----------------------
+// 1. Create function that runs on page load.
+// 2. Use Moment.js and use correct format to display current day.
+// 3. Inside of this function, append current date to <p id=currentDayInWeek>
+
+var currentDay = moment().format();
+
+function displayDay(){
+    
+};
+
+
+
+
+
+
+
+// Display timeblocks for biz hours (9a-5p). One block per hour
+// ----------------------
+// 1. Create object holding a timeblock for each hour
+// 2. Use object properties to ... 
+
+
+// Clicking timeblock allows for user input of an event
+// ----------------------
+// 1. Add click event to timeblock.
+// 2. Define jQuery modal and provide the required inputs.
+// 3. On click, display the jQuery modal to collect user inputs.
+
+// Clicking 'save' stores the text in local storage
+// ----------------------
+// 1. Create button inside of modal titled "save".
+// 2. Add click event to button.
+// 3. On click, save user input to local storage.
+// 4. On click, close the modal.
+
+
+// If app is refreshed, these events remain in place
+// ----------------------
+// 1. 
+
+// Style timeblocks based on past, present, or future.
+// ----------------------
+// 1. 
